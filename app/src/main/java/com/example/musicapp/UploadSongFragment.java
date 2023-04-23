3e4220044ce885a6c40544b3ab3b9688599929d1
ddrokan <ddrokan@ddrokan.lan.croz.net>
2023-04-23 13:00:58 +0200

    Upload song use case, ne radi jos kako treba navbar je drukciji kad su 4 item-a u njemu

--- app/src/main/java/com/example/musicapp/UploadSongFragment.java
+++ app/src/main/java/com/example/musicapp/UploadSongFragment.java
@@ -1,28 +1,39 @@
 package com.example.musicapp;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.DocumentsContract;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatImageButton;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.textview.MaterialTextView;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
 public class UploadSongFragment extends Fragment {
 
-    private Button browseBtn;
+    private Button browseBtn, uploadBtn;
+    private String songPath;
     public UploadSongFragment() {
         // Required empty public constructor
     }
@@ -39,22 +50,64 @@ public class UploadSongFragment extends Fragment {
     }
 
     @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 10){
+            String path = data.getData().getEncodedPath();
+
+            Log.d("path", path);
+            songPath = String.valueOf(path);
+        }
+    }
+
+    @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
         MainActivity activity = (MainActivity) getActivity();
         assert activity != null;
 
+        /*
+        MaterialToolbar toolbar = view.findViewById(R.id.top_bar);
+        toolbar.setNavigationOnClickListener(v -> activity.getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.main_fragment_container, activity.homeFragment)
+                .commit());
+         */
+
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef = storage.getReference();
+
         browseBtn = view.findViewById(R.id.fragment_upload_song_browse_btn);
         browseBtn.setOnClickListener(v -> {
-            Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
-            intent.addCategory(Intent.CATEGORY_OPENABLE);
-            intent.setType("application/pdf");
+            // Define the intent to open the file picker
+            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+            intent.setType("*/*"); // Set the MIME type of the files to select
 
-            // Optionally, specify a URI for the file that should appear in the
-            // system file picker when it loads.
+            startActivityForResult(intent, 10);
 
-            startActivity(intent);
+        });
+
+        uploadBtn = view.findViewById(R.id.fragment_upload_upload_song_btn);
+        uploadBtn.setOnClickListener(v ->{
+            StorageReference song = storageRef.child("song.mp3");
+            File songFile = new File(songPath);
+            Log.d("upload", songFile.toString());
+            Uri file = Uri.fromFile(new File(songPath));
+            UploadTask uploadTask = song.putFile(file);
+            // Register observers to listen for when the download is done or if it fails
+            uploadTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    // Handle unsuccessful uploads
+                }
+            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
+                    // ...
+                }
+            });
         });
 
 
